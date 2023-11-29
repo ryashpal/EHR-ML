60daedc7ce2b32ef03870d450d701885165b68ef
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-11-29 15:44:11 +1100

    add anchor column as input parameter

--- ehrml/predict_mortality/Evaluate.py
+++ ehrml/predict_mortality/Evaluate.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 log = logging.getLogger("EHR-ML")
@@ -12,18 +13,23 @@ from ehrml.utils import DataUtils
 from ehrml.utils import MlUtils
 
 
-def run(dirPath, idColumns, targetColumn, measurementDateColumn, windowStart, windowEnd, savePath):
+def run(dirPath, idColumns, targetColumn, measurementDateColumn, anchorDateColumn, windowStart, windowEnd, savePath):
     data = DataUtils.readData(
         dirPath=dirPath,
         idColumns=idColumns,
         targetColumn=targetColumn,
         measurementDateColumn=measurementDateColumn,
+        anchorDateColumn=anchorDateColumn,
         windowStart=windowStart,
         windowEnd=windowEnd,
         )
     X, XVitalsAvg, XVitalsMin, XVitalsMax, XVitalsFirst, XVitalsLast, XLabsAvg, XLabsMin, XLabsMax, XLabsFirst, XLabsLast, y, idsDf = data
 
     xgbEnsembleScores = MlUtils.evaluateEnsembleXGBoostModel(X, XVitalsAvg, XVitalsMin, XVitalsMax, XVitalsFirst, XVitalsLast, XLabsAvg, XLabsMin, XLabsMax, XLabsFirst, XLabsLast, y[args.target_column[0]])
+
+    dirPath = Path(savePath).parent
+    if not os.path.exists(dirPath):
+        os.makedirs(dirPath)
     savePath = Path(savePath)
     DataUtils.saveCvScores(xgbEnsembleScores, savePath.parent, savePath.name)
 
@@ -58,6 +64,9 @@ if __name__ == '__main__':
     parser.add_argument('-mdc', '--measurement_date_column', nargs=1, default=['measurement_date'],
                         help='Name of the column containing the measurement date')
 
+    parser.add_argument('-adc', '--anchor_date_column', nargs=1, default=['visit_start_datetime'],
+                        help='Name of the anchor date column')
+
     parser.add_argument('-wb', '--window_before', nargs=1, type=int, default=0,
                         help='Number of days or data to include before time-zero. By default: [window_before=0]')
 
@@ -73,6 +82,7 @@ if __name__ == '__main__':
     log.info('args.target_column: ' + str(args.target_column[0]))
     log.info('args.id_columns: ' + str(args.id_columns))
     log.info('args.measurement_date_column: ' + str(args.measurement_date_column[0]))
+    log.info('args.anchor_date_column: ' + str(args.anchor_date_column[0]))
     log.info('args.window_before: ' + str(args.window_before[0]))
     log.info('args.window_after: ' + str(args.window_after[0]))
     log.info('args.save_path: ' + str(args.save_path[0]))
@@ -81,6 +91,7 @@ if __name__ == '__main__':
         dirPath=args.data_file[0],
         idColumns=args.id_columns,
         targetColumn=args.target_column[0],
+        anchorDateColumn=args.anchor_date_column[0],
         measurementDateColumn=args.measurement_date_column[0],
         windowStart=args.window_before[0],
         windowEnd=args.window_after[0],
