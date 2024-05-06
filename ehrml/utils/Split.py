6603b544523cbb1eb9113ce161116e568b5cc320
Yashpal Ramakrishnaiah <ryashpal89@gmail.com>
2024-05-06 06:16:48 +0000

    pushing local changes

--- ehrml/utils/Split.py
+++ ehrml/utils/Split.py
@@ -2,10 +2,13 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-def split_random(dataDf, anchorDateColumn, measurementDateColumn, windowStart, windowEnd, testSize, shuffle, seed):
+def split_random(dataDf, idColumns, anchorDateColumn, measurementDateColumn, anchorDateColumnFormat='%d/%m/%Y', measurementDateColumnFormat='%d/%m/%Y', windowStart=0, windowEnd=0, testSize=0.8, shuffle=True, seed=42):
 
-    dataDf[anchorDateColumn] = pd.to_datetime(dataDf[anchorDateColumn], format='%d/%m/%Y')
-    dataDf[measurementDateColumn] = pd.to_datetime(dataDf[measurementDateColumn], format='%d/%m/%Y')
+    dataDf[anchorDateColumn] = pd.to_datetime(dataDf[anchorDateColumn], format=anchorDateColumnFormat)
+    dataDf[measurementDateColumn] = pd.to_datetime(dataDf[measurementDateColumn], format=measurementDateColumnFormat)
     dataDf = dataDf[(dataDf[measurementDateColumn] >= (dataDf[anchorDateColumn] - pd.Timedelta(days=windowStart))) & (dataDf[measurementDateColumn] <= (dataDf[anchorDateColumn] + pd.Timedelta(days=windowEnd)))]
-    xTestDataDf, xTrainDataDf = train_test_split(dataDf, test_size=testSize, shuffle=shuffle, seed=seed)
-    return xTestDataDf, xTrainDataDf
+    idsDf = dataDf[idColumns].drop_duplicates()
+    testIdsDf, trainIdsDf = train_test_split(idsDf, test_size=testSize, shuffle=shuffle, random_state=seed)
+    testDataDf = dataDf.merge(testIdsDf, how='inner', on=idColumns)
+    trainDataDf = dataDf.merge(trainIdsDf, how='inner', on=idColumns)
+    return testDataDf, trainDataDf
